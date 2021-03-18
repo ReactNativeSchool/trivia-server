3e480da050b19b625dd0ae664ac68d0a583eeb57
Spencer Carli <spencercarli@gmail.com>
2021-03-18 10:59:28 -0500

    04 Connect to MongoDB

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,6 +1,13 @@
+import mongoose from "mongoose";
+import { connectMongo } from "../../util/dbConnect";
+
 export const resolvers = {
   Query: {
-    greet: () => {
+    greet: async () => {
+      console.log("before", mongoose.connection.readyState);
+      await connectMongo();
+      console.log("after", mongoose.connection.readyState);
+
       return {
         name: "Jane Doe",
         message: "Hello, world!",
---
+++ util/dbConnect.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+export const connectMongo = async () => {
+  if (mongoose.connection.readyState >= 1) {
+    return;
+  }
+
+  return mongoose.connect(process.env.DB_CONNECTION, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  });
+};
