b10fe80b972dd555231d2963cc7b5aab5caf032b
Spencer Carli <spencercarli@gmail.com>
2021-03-30 10:25:48 -0500

    06 Fetch Store Questions

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,5 +1,8 @@
 import mongoose from "mongoose";
+import { unescape } from "lodash";
+
 import { connectMongo } from "../../util/dbConnect";
+import { Question } from "../../models/Question";
 
 export const resolvers = {
   Query: {
@@ -10,6 +13,39 @@ export const resolvers = {
         name: "Jane Doe",
         message: "Hello, world!",
       };
+    },
+  },
+
+  Mutation: {
+    fetchQuestions: async () => {
+      await connectMongo();
+
+      const url = "https://opentdb.com/api.php?amount=3";
+      const response = await fetch(url);
+      const { results } = await response.json();
+
+      const formattedResults = results.map((result) => {
+        return {
+          ...result,
+          question: unescape(result.question),
+          answers: [
+            {
+              answer: result.correct_answer,
+              correct: true,
+            },
+            ...result.incorrect_answers.map((answer) => {
+              return {
+                answer: answer,
+                correct: false,
+              };
+            }),
+          ],
+        };
+      });
+
+      const insertResult = await Question.insertMany(formattedResults);
+
+      return { questionsSaved: insertResult.length };
     },
   },
 };
--- graphql/schemas/index.js
+++ graphql/schemas/index.js
@@ -5,8 +5,16 @@ export const typeDefs = gql`
     greet: ExampleResponse
   }
 
+  type Mutation {
+    fetchQuestions: FetchQuestionsResponse
+  }
+
   type ExampleResponse {
     name: String
     message: String
+  }
+
+  type FetchQuestionsResponse {
+    questionsSaved: Int
   }
 `;
